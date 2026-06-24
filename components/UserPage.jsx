49228650abe057e774b446274c5b7a766bbf3b4e
João Pedro Ribeiro Turchi <2024108102540001@ifgoiano.local>
2026-06-24 09:54:06 -0300

    Começando a desenvolver a UserPage

--- components/UserPage.jsx
+++ components/UserPage.jsx
@@ -2,8 +2,16 @@
 
 export default function UserPage(){
     return(
-        <div>
-            <p>Hello World</p>
+        <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center text-black">
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign Up</h2>
+            </div>
+            <div>
+                <form method="POST" className="space-y-5">Username:</form>
+                <div>
+                    <label htmlFor=""></label>
+                </div>
+            </div>
         </div>
     )
 }
